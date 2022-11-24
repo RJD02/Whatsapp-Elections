f3dc34966262ba53ff00f87ab30dbf4341a9ca22
RJD02 <dulangeraviraj@gmail.com>
2022-11-24 21:59:06 +0530

    Update

---
+++ app.js
@@ -0,0 +1,65 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const axios = require("axios");
+
+app.use(express().json());
+
+const port = process.env.PORT || 3000;
+
+const VERIFY_TOKEN = "c5fcf44f-5faa-4a46-ad78-1504fa531781";
+
+app.get("/webhook", (req, res) => {
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  if (mode && token) {
+    if (mode == "subscribe" && token == VERIFY_TOKEN) {
+      console.lof("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
+});
+
+app.post("/webhook", (req, res) => {
+  const body = req.body;
+  console.log(JSON.stringify(body, null, 2));
+  if (req.body.object) {
+    if (
+      body.entry &&
+      body.entry[0].changes &&
+      body.entry[0].changes[0] &&
+      body.entry[0].changes[0].value.messages &&
+      body.entry[0].changes[0].value.messages[0]
+    ) {
+      let phone_number_id =
+        req.body.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = req.body.entry[0].changes[0].value.messages[0].from;
+      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body;
+      axios({
+        method: "POST",
+        url:
+          "https://graph.facebook.com/v12.0/" +
+          phone_number_id +
+          "/messages?access_token=" +
+          process.env.WHATSAPP_TOKEN,
+        data: {
+          messaging_product: "whatsapp",
+          to: from,
+          text: { body: "Ack: " + msg_body },
+          headers: { "Content-Type": "application/json" },
+        },
+      });
+    }
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.listen(port, () => {
+  console.log("Server listening on port", port);
+});
