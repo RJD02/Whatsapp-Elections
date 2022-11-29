f833e238cac15e723efbc69af32663d9e043acf1
RJD02 <dulangeraviraj@gmail.com>
2022-11-29 14:14:34 +0530

    Add voters model

---
+++ models/voters.js
@@ -0,0 +1,33 @@
+/*
+
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", UserSchema);
+
+*/
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const VoterSchema = new Schema({
+  Ward_no: String,
+  SLNO: String,
+  houseno: String,
+  VNAME_ENGLISH: String,
+  cardno: String,
+  Age: Number,
+});
+
+module.exports = mongoose.model("Voter", VoterSchema);
