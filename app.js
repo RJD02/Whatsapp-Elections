2b2b7b5f4fa37e9100ee1b22b2e297f44e0a19d2
RJD02 <dulangeraviraj@gmail.com>
2022-12-03 14:20:18 +0530

    Remove access requests

--- app.js
+++ app.js
@@ -19,10 +19,6 @@ db.once("open", () => {
   console.log("Database connected");
 });
 app.use(express.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  next();
-});
 
 const port = process.env.PORT || 3000;
 
