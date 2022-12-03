5c582bb60c93d2906cc8db8f1dedbe937b8f700a
RJD02 <dulangeraviraj@gmail.com>
2022-12-03 20:51:09 +0530

    Add check for message body

--- app.js
+++ app.js
@@ -56,7 +56,9 @@ app.post("/webhook", async (req, res) => {
       let phone_number_id =
         req.body.entry[0].changes[0].value.metadata.phone_number_id;
       let from = req.body.entry[0].changes[0].value.messages[0].from;
-      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body;
+      let msg_body = "";
+      if (req.body.entry[0].changes[0].value.messages[0].text.body)
+        msg_body = req.body.entry[0].changes[0].value.messages[0].text.body;
       let voter = null;
       console.log("This is the card number", msg_body);
       try {
