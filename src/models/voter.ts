d0e7f67ca1c47b5fc42746eb52deaaf299c0321f
RJD02 <dulangeraviraj@gmail.com>
2023-01-19 04:46:51 +0530

    Add voter model in ts

---
+++ src/models/voter.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const VoterSchema = new Schema({
+  Ward_no: String,
+  SLNO: String,
+  houseno: String,
+  VNAME_ENGLISH: String,
+  cardno: String,
+  Age: Number,
+  PreferredLanguage: String,
+  mobileNumber: String,
+});
+
+export const Voter = mongoose.model("Voter", VoterSchema);
