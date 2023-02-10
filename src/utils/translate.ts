83aecdf8077d0fb9226ad66108ec1b4d3677dad1
RJD02 <dulangeraviraj@gmail.com>
2023-02-10 16:11:50 +0530

    Change translate package

--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -2,11 +2,14 @@ import { languageMappings } from "./languageMappings";
 import { Translate } from "free-translate";
 import { Language } from "free-translate/lib/types/Language";
 import { translate } from "free-translate";
+import translatte from "translatte";
 
 export const translateText = async (msg: string, to: Language) => {
   try {
-    const translatedText = await translate(msg, { from: "en", to: to });
-    return translatedText;
+    // const translatedText = await translate(msg, { from: "en", to: to });
+    const translated = await translatte(msg, { from: "en", to });
+    const text = await translated.text;
+    return text;
   } catch (e) {
     console.log("Error while translating");
     console.log(e);
